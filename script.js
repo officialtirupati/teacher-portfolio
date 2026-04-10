d5b3c400dc4bc2fa6ed7da36777a673382be8d72
officialtirupati <officialtirupati@gmail.com>
2026-04-10 18:18:11 +0530

    script.js

--- script.js
+++ script.js
@@ -5,7 +5,34 @@ function scrollToSection(id) {
   });
 }
 
-// Form message
+// Form
+const scriptURL = "https://script.google.com/macros/s/AKfycbzLX1T7dp7AltAR7fXTsqnhKkB3B0vSCebVx0pODyWcVUDOBVWqOlkirapJNxQpcX2sVQ/exec";
+
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const data = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value
+  };
+
+  fetch(scriptURL, {
+    method: "POST",
+    body: JSON.stringify(data)
+  })
+  .then(res => res.json())
+  .then(response => {
+    document.getElementById("successMsg").innerText =
+      "✅ Message sent successfully!";
+    document.getElementById("contactForm").reset();
+  })
+  .catch(error => {
+    document.getElementById("successMsg").innerText =
+      "❌ Something went wrong!";
+  });
+});
+
 
 
 
